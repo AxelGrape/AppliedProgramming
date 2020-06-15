ee522c3df2a3b15f745b45511f5b75d5fe88e10e
axelpett101@student.kau.se <axelpett101@student.kau.se>
2020-06-15 15:38:23 +0200

    More basic stuff

--- interface_module.py
+++ interface_module.py
@@ -3,3 +3,10 @@ from parser_module import parse
 
 def test_parsing(file_name):
     parse(get_lexeme_list(file_name))
+
+
+def __parse_file_output(file_name):
+    return parse(get_lexeme_list(file_name))
+
+def parse_file(file_name):
+    return __parse_file_output(file_name)
--- parser_module.py
+++ parser_module.py
@@ -1,17 +1,19 @@
 from lexer_module import associate_token
 
 parse_ok = 1
+output_log = "Start parse :"
 
 def __match_next(lexeme_list, expected):
     current_token = associate_token(lexeme_list.pop(0))
-    if(current_token == expected):
-        print(f'Expected: {expected} found: {current_token}')
-    else:
-        print(f'Syntax error: Expected: {expected} found: {current_token}')
+    if(current_token != expected):
+        global output_log
+        output_log += f'\n\tExpected: {expected} found: {current_token}'
+        #print(f'Syntax error: Expected: {expected} found: {current_token}')
         parse_ok = 0
 
 def parse(lexeme_list):
     __parse_program_header(lexeme_list)
+    return output_log
 
 def __parse_program_header(lexeme_list):
     __match_next(lexeme_list, "program")
@@ -22,4 +24,5 @@ def __parse_program_header(lexeme_list):
     __match_next(lexeme_list, "output")
     __match_next(lexeme_list, ")")
     __match_next(lexeme_list, ";")
+    print(output_log)
     return parse_ok
