bb5990c83e0e0909c4ed6a9b8c822e40a2b09705
AxelGrape <41511588+AxelGrape@users.noreply.github.com>
2020-07-03 15:15:59 +0200

    Update parser_module.py
    More grammar rules, attempted adding error handling

--- parser/parser_module.py
+++ parser/parser_module.py
@@ -1,11 +1,14 @@
-from lexer_module import associate_token
+from lexer_module import associate_token, get_last_lexeme
+from symbol_table_module import SymbolTable
 
 parse_ok = 1
 line = 1
 output_log = ""
+symb_table = SymbolTable([])
+
 
 def __lookahead(lexeme_list, str):
-    return True if associate_token(lexeme_list[0]) ==  str else False
+    return associate_token(lexeme_list[0]) if associate_token(lexeme_list[0]) ==  str else False
 
 
 
@@ -18,13 +21,24 @@ def __match_next(lexeme_list, expected):
         current_token = associate_token(lexeme_list.pop(0))
 
     if(current_token != expected):
-        global output_log
-        output_log += f'\n\tExpected: {expected} found: {current_token} on line {line}'
-        print(f'Syntax error: Expected: {expected} found: {current_token} on line {line}')
-        parse_ok = 0
+        print(f'Syntax error: Expected: {expected} found: {get_last_lexeme()} on line {line}')
+        __expected_error(expected, current_token, line)
     else:
         print(f'Expected: {expected} found: {current_token}')
 
+
+#Error handling
+    def __expected_error(expected, found, line):
+        global output_log
+        output_log += f'\n\tExpected: {expected} found: {current_token} on line {line}'
+        global parse_ok
+        parse_ok = 0
+
+    def __name_found_error(id):
+        global parse_ok
+        parse_ok = 0
+        global output_log
+        output_log += f'\n\tSEMANTIC: ID already declared: {id}'
 
 # Grammar rules
 
@@ -57,18 +71,33 @@ def __var_dec(lexeme_list):
     __match_next(lexeme_list, ";")
 
 def __id_list(lexeme_list):
+    lexeme_value = __lookahead(lexeme_list, "ID") #To update last_lexeme, will make this better later
+    global symb_table
+    if(symb_table.in_symbol_table(get_last_lexeme())):
+        __name_found_error(get_last_lexeme())
+    elif(lexeme_value == "ID"):
+        symb_table.add_to_table(get_last_lexeme(), "var")
+
     __match_next(lexeme_list, "ID")
     if(__lookahead(lexeme_list, ",")):
         __match_next(lexeme_list, ",")
         __id_list(lexeme_list)
 
 def __type(lexeme_list):
+    type = "undefined"
     if __lookahead(lexeme_list, "integer"):
          __match_next(lexeme_list, "integer")
+         type = "integer"
     if __lookahead(lexeme_list, "real"):
          __match_next(lexeme_list, "real")
+         type = "real"
     if __lookahead(lexeme_list, "boolean"):
          __match_next(lexeme_list, "boolean")
+         type = "boolean"
+    global symb_table
+    symb_table.update_types(type)
+
+
 
 
 # Public
@@ -76,8 +105,11 @@ def __type(lexeme_list):
 def parse(lexeme_list):
     __parse_program_header(lexeme_list)
     __parse_var_part(lexeme_list)
+    global symb_table
+    symb_table.print_symbol_table()
+
     if len(output_log) < 1:
-        print("Parse Completed!")
+        print("Parse Completed")
     else:
         print("Parse failed! See output log")
     return output_log
